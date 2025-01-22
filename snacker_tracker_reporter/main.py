06cc38f31dc543e1cdbc0ef8ea2738a329353646
Laurence MacGuire <leprechaun@gmail.com>
2025-01-22 17:16:43 +0700

    functional, but aider did not want to commit

--- snacker_tracker_reporter/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.post("/v1/scans/", status_code=201)
-def create_scan():
-    return {}
--- tests/test_scans.py
+++
@@ -1,8 +0,0 @@
-from fastapi.testclient import TestClient
-from ..main import app
-
-client = TestClient(app)
-
-def test_create_scan():
-    response = client.post("/v1/scans/")
-    assert response.status_code == 201
